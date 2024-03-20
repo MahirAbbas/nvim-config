14f521f950d1738685d127f1b304035616126023
mahir <mabbasest@mahirabbas.com>
2024-03-20 12:25:46 +0000

    update

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.wo.number = true
+vim.wo.relativenumber = true
 
 --- lazy setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
